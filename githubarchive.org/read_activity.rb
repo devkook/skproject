da7117c0a87771ffa9cd5be68118cfee8a256a16
devkook <devinkook@gmail.com>
2014-09-21 18:04:57 +0900

    추출

--- githubarchive.org/read_activity.rb
+++ githubarchive.org/read_activity.rb
@@ -6,6 +6,7 @@ gz = open('http://data.githubarchive.org/2012-03-11-12.json.gz')
 js = Zlib::GzipReader.new(gz).read
  
 Yajl::Parser.parse(js) do |event|
-  jsonstr = event
-  puts event
+  url = event["repository"]["url"]
+  #forks = event["repository"]["forks"]
+  puts "#{url}"
 end
--- githubarchive.org/read_activity_edit.rb
+++ githubarchive.org/read_activity_edit.rb
@@ -1,11 +1,34 @@
 require 'open-uri'
 require 'zlib'
 require 'yajl'
- 
-gz = open('http://data.githubarchive.org/2012-03-11-12.json.gz')
-js = Zlib::GzipReader.new(gz).read
- 
-Yajl::Parser.parse(js) do |event|
-  jsonstr = event
-  puts event
+
+def readsave(date)
+	gz = open("http://data.githubarchive.org/#{date}.json.gz")
+	js = Zlib::GzipReader.new(gz).read
+
+	# file = File.open("Users/diginori/study/skproject/githubarchive.org/#{date}.log", "w")
+
+	Yajl::Parser.parse(js) do |event|
+	  
+	  begin
+	    pushed_at = event["repository"]["pushed_at"]
+		url = event["repository"]["url"]
+		forks = event["repository"]["forks"]
+
+		# file.write("#{pushed_at},#{url},#{forks}")
+		puts "#{pushed_at},#{url},#{forks}"
+	  rescue
+	    puts "ERR"
+	  end
+	  
+	end
+
+	# file.close unless file == nil
+
+	puts "#{date}"
 end
+
+for date in "2012-04-11-1".."2012-04-12-23"
+	readsave(date)
+end
+
